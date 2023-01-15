460a155e73c40928efd13d3d98449c51026ecd0f
Matteo Zucca <Matwbrody@gmail.com>
2023-01-15 12:42:41 +0100

    Rimosso classe e API favourites da NODE

--- Node/app/src/routes/routes.js
+++ Node/app/src/routes/routes.js
@@ -1,13 +1,8 @@
 import express from "express";
-import { addFavourite, deleteFavourite, getFavourites, updateFavourite } from "../controllers/favourite-controller.js";
 import { addScore, deleteScore, getScore, top10, updateScore } from "../controllers/score-controller.js";
 
 const router = express.Router();
 
-router.get(`/favouritesOfUser/:userId`, getFavourites);
-router.post(`/favourite`, addFavourite);
-router.patch(`/favourite/:id`, updateFavourite);
-router.delete(`/favourite/:id`, deleteFavourite);
 router.get(`/scoresOfUser/:userId`, getScore);
 router.get(`/top10`, top10);
 router.post(`/score`, addScore);
